0b34b856bffe106c692b19df7b5cc3d6f24e788b
Marcello De Salvo <marcello.desalvo@mail.polimi.it>
2023-01-27 17:46:43 +0100

    - fixed navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -184,7 +184,7 @@ export default {
     return { logout }
   },
   mounted(){
-    if (Cookies.get('token') != '')
+    if (Cookies.get('token') != '' || Cookies.get('token') != null)
         this.logged = true;
     else
         this.logged = false;
