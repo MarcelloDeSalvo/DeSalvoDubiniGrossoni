fedcccc15faa398087725be177f32bb4f796a53b
Marcello De Salvo <marcello.desalvo@mail.polimi.it>
2023-01-27 16:24:03 +0100

    - updated makebooking url reference

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
     <Googlemap />
 
     <div class='flex items-center justify-center py-10'>
-        <a href="/makebooking" class="flex px-3 py-2 bg-blue-400 mr-1 text-white font-semibold rounded">
+        <a href="/makebooking/0/" class="flex px-3 py-2 bg-blue-400 mr-1 text-white font-semibold rounded">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
